28131ddf22de1c0e9aa4c32e298e44b5907faa49
Bruno Braat <bruno.braat@gmail.com>
2023-04-27 10:54:24 -0300

    Adicionando o atributo e metodo de idade,

--- projetojava1/src/curso/java/braat/Pessoa.java
+++ projetojava1/src/curso/java/braat/Pessoa.java
@@ -2,25 +2,36 @@ package curso.java.braat;
 
 public class Pessoa {
 
+	/*Atributos*/
 	private String nome;
 	
 	private String sobrenome;
 
+	private int idade;
+	
+	/*Metodos*/
 	public String getNome() {
 		return nome;
 	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
+	
 	public String getSobrenome() {
 		return sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
 	
-	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
 }
